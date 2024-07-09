04f867d3f444a71c378fd20a4a13d2a3473d6672
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2024-07-09 14:21:53 +0300

    feat: add some styles to the Header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,9 @@
-import Link from "next/link";
+import Header from "@/components/layout/Header";
 
 export default function Home() {
   return (
     <>
-      <header className="flex items-center justify-between">
-        <Link className="text-primary font-semibold text-2xl" href={""}>
-          ST PIZZA
-        </Link>
-        <nav className="flex gap-4">
-          <Link href={""}>Home</Link>
-          <Link href={""}>Menu</Link>
-          <Link href={""}>About</Link>
-          <Link href={""}>Contact</Link>
-          <Link href={""}>Login</Link>
-        </nav>
-      </header>
+      <Header />
     </>
   );
 }
