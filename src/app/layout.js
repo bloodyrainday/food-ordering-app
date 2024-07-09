a86e538025d1ac622d5bee5ea0559c6a41b4fafc
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2024-07-09 14:11:58 +0300

    feat: add nav section

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <main className="max-w-4xl border mx-auto">{children}</main>
+        <main className="max-w-4xl border mx-auto p-4">{children}</main>
       </body>
     </html>
   );
