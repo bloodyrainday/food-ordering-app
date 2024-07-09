bc053677efcc26bb772cf87e39ff63dec1e9e2fe
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2024-07-09 16:32:10 +0300

    feat: add some styles to the Hero component

---
+++ src/components/layout/Hero.js
@@ -0,0 +1,41 @@
+import React from "react";
+import pizzaImage from "../../assets/pizza.png";
+
+import Image from "next/image";
+import RightArrow from "../icons/RightArrow";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="py-12">
+        <h1 className="text-4xl font-semibold mb-6">
+          Everything <br /> is better <br />
+          with a <span className="text-primary">Pizza</span>
+        </h1>
+        <p className="text-gray-500 mb-6 text-sm">
+          Pizza is the missing piece that makes every day complete, a simple yet
+          delicious joy in life
+        </p>
+        <div className="flex text-sm">
+          <button className="flex gap-2 bg-primary text-white px-4 py-2 rounded-full uppercase items-center">
+            Order now <RightArrow />
+          </button>
+          <button className="flex gap-2 font-semibold text-gray-600 px-4 py-2 rounded-full">
+            Learn now <RightArrow />
+          </button>
+        </div>
+      </div>
+
+      <div className="relative">
+        <Image
+          src={pizzaImage}
+          alt={"pizza-img"}
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
