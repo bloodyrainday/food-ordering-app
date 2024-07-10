f085945c9e45deb9d37fc49ed5061ced62377f83
mikita bialko <104152177+bloodyrainday@users.noreply.github.com>
2024-07-10 11:27:58 +0300

    feat: add Contact section in the Page component

--- src/app/page.js
+++ src/app/page.js
@@ -31,6 +31,21 @@ export default function Home() {
           </p>
         </div>
       </section>
+
+      <section className="text-center my-8" id="contact">
+        <SectionHeaders
+          subHeader={"Don't hesitate"}
+          mainHeader={"Contact us"}
+        />
+        <div className="mt-8">
+          <a
+            className="text-4xl underline text-gray-500"
+            href="tel:+46738123123"
+          >
+            +46 738 123 123
+          </a>
+        </div>
+      </section>
     </>
   );
 }
